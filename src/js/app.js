e3c6f12829ea33629260a78fe66e23154aef9297
max <maks@coderiver.com.ua>
2019-03-11 09:05:13 +0200

    100vh

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,10 @@ import './components/slider';
 import './components/tabs';
 import './components/modal';
 import './components/scrollTo';
+
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
