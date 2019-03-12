64fdfa814fb6a7488b82d9db84f37166ca95d5c9
max <maks@coderiver.com.ua>
2019-03-12 10:24:00 +0200

    upd

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,5 +2,13 @@ import 'slick-carousel';
 
 $('.js-slider').slick({
   prevArrow: '<button class="btn-slick btn-slick--prev" type="button"><svg class="icon icon-left"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-left"></use></svg></button>',
-  nextArrow: '<button class="btn-slick btn-slick--next" type="button"><svg class="icon icon-right"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-right"></use></svg></button>'
+  nextArrow: '<button class="btn-slick btn-slick--next" type="button"><svg class="icon icon-right"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/sprite.svg#icon-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
 });
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -5,13 +5,15 @@ tabBtn.on('click', (e) => {
   const parent = $(e.currentTarget).parents('.js-tabs');
   const index = $(e.currentTarget).data('tab');
   if ($(e.currentTarget).hasClass('is-active')) {
-    $(e.currentTarget).removeClass('is-active');
+    tabBtn.removeClass('is-active is-unactive');
     tabs.slideUp();
   } else {
-    tabBtn.removeClass('is-active');
+    tabBtn.removeClass('is-active is-unactive');
     tabs.slideUp();
     $(e.currentTarget).addClass('is-active');
     const activeTab = parent.find(`.js-tab[data-tab="${index}"]`);
+    const unactiveBtns = parent.find('.js-tab-title:not(.is-active)');
+    unactiveBtns.addClass('is-unactive');
     const activeContent = parent.find('.js-tab-content');
     activeTab.slideDown();
     setTimeout(() => {
